5fc77f76a60e6b853c8da3c25bc1e9e5aaf92ffe
teklemariam <teklemariammossie1@gmail.com>
2023-07-07 18:15:34 +0000

    lockbox

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -3,14 +3,24 @@
 open other lockboxes
 """
 def canUnlockAll(boxes):
-    unlocked = set()
+    """A function that checks if all boxes can be opened"""
+    keys = []  # List to store unique keys to unlock boxes
+    length = len(boxes)
+    idx = 0
 
-    for box_id, box in enumerate(boxes):
-        if len(box) == 0 or box_id == 0:
-            unlocked.add(box_id)
-        for key in box:
-            if key < len(boxes) and key != box_id:
-                unlocked.add(key)
-        if len(unlocked) == len(boxes):
-            return True
-    return False
+    # Iterate over the boxes
+    for i in boxes:
+        # Iterate over numbers in each boxes
+        for j in i:
+            # Store unique numbers from boxes as keys
+            if j not in keys and j != 0 and j != idx:
+                keys.append(j)
+        idx += 1
+    # Sort the keys in ascending order
+    keys = sorted(keys)
+    # Iterate over the range of the length of the key, and make sure that each
+    # box is opened by the keys.
+    for k in range(1, length):
+        if k != keys[k - 1]:
+            return False
+    return True
