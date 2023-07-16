6c5cb50ca240eb222bb5ac135063541b49467e5a
teklemariam <teklemariammossie1@gmail.com>
2023-07-16 08:52:46 +0000

    minimum operation

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
 
+"""Minimum Operations are those activities necessary for the 
+business or operation to maintain the condition of facilitiy.
+"""
 def minOperations(n):
+	"""Returns the fewest number of operations needed 
+	to result in exactly n H characters in the file.
+	"""
+	
 	min = 2
 	operation = 0
 	while n > 1:
