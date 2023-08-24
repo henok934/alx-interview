4a55c482f3c3d410ae41086396b8f5206993f87b
teklemariammossie1@gmail.com <teklemariammossie1@gmail.com>
2023-08-24 16:29:06 -0400

    updated

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,29 +1,37 @@
 #!/usr/bin/python3
 """
-Validate UTF-8 encoded text.
+File: 0-validate_utf8.py
 """
 
 
 def validUTF8(data):
     """
-    Check if data os a valid UTF-8 encoding
-
-    Args:
-        data: A list of bytes.
-
-    Returns:
-        if data is correctly encode to utf 8 True, otherwise False.
+    determines if a given data set represents a valid UTF-8 encoding.
     """
-    for byte in data:
-        if byte >> 7 == 0:
-            continue
-        elif byte >> 5 == 0b110:
-            continue
-        elif byte >> 4 == 0b1110:
-            continue
-        elif byte >> 3 == 0b11110:
-            continue
-        else:
-            return False
-    return True
+    number_bytes = 0
 
+    mask1 = 1 << 7
+    mask2 = 1 << 6
+
+    for i in data:
+
+        mask_n_byte = 1 << 7
+
+        if number_bytes == 0:
+            while mask_n_byte & i:
+                number_bytes += 1
+                mask_n_byte = mask_n_byte >> 1
+            if number_bytes == 0:
+                continue
+
+            if number_bytes == 1 or number_bytes > 4:
+                return False
+
+        else:
+            if not (i & mask1 and not (i & mask2)):
+                return False
+        number_bytes -= 1
+    if number_bytes == 0:
+        return True
+
+    return False
