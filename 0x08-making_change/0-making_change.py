75f5e7357cede5e2775c612e9710e59b887225db
teklemariammossie1@gmail.com <teklemariammossie1@gmail.com>
2023-09-23 14:20:04 -0400

    0-making_change.py

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -3,20 +3,15 @@
 
 
 def makeChange(coins, total):
-    # Initialize the array to store the minimum number of coins
     dp = [float('inf')] * (total + 1)
-    dp[0] = 0  # Base case: 0 coins needed to make amount 0
+    dp[0] = 0
 
-    # Iterate over all amounts from 1 to the total
     for amount in range(1, total + 1):
-        # Try each coin denomination
         for coin in coins:
             if coin <= amount:
-                # Update the minimum number of coins needed
                 dp[amount] = min(dp[amount], dp[amount - coin] + 1)
 
-    # Check if the total amount can be made using the given coins
     if dp[total] == float('inf'):
-        return -1  # Total cannot be met by any number of coins
+        return -1
     else:
-        return dp[total]  # Return the minimum number of coins needed
+        return dp[total]
